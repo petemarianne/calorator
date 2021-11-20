392c0a209be6526004e5eeb5bff5baf456aec650
Pete Marianne <petemarianne@Petes-MacBook-Pro.local>
2021-11-21 00:52:50 +0300

    create an empty app

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+//import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import './App.scss';
+import './styles/buttons.scss';
+
+
+const App: React.FC = (): JSX.Element => {
+    return (
+        <div className='App'>
+        </div>
+    );
+};
+
+export default App;
